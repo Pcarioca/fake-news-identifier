c23b7b2ccb202ff01b56049d8ff3d2c5a4135aba
Pcarioca <63117204+Pcarioca@users.noreply.github.com>
2020-06-16 23:40:54 +0300

    Functioneaza!
    Am adaugat butonul arata local Storage, am facut styling si am rezolvat cu todo.

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
- 
+document.addEventListener('DOMContentLoaded', function () {
 
-   var todo = [];
-   document.getElementById("buton").onclick = add();
-   var i=0;
+
+
+    var todo = [];
+    document.getElementById("buton").onclick = add;
     
-    function add(){
-        alert("Am intrat in functie");
-        var da=document.getElementById("input").value;
-        todo.push(da);
-        
-        var para = document.createElement("p");
-        var node = document.createTextNode(todo[i]);
-        para.appendChild(node);
-        var element = document.getElementById("div1");
-        element.appendChild(para);
-        i++;
-        
-      
+    var i = 0;
+    var cookie="";
+    function add() {
+
+        var da = document.getElementById("input").value;
+        if (da.localeCompare("")) {
+            todo.push(da);
+            var para = document.createElement("p");
+            var node = document.createTextNode(todo[i]);
+            para.appendChild(node);
+            var element = document.getElementById("div1");
+            element.appendChild(para);
+            var br = document.createElement("br");
+            element.appendChild(br);
 
 
-        
+            i++;
+            document.getElementById("input").value = "";
+
+            
 
 
-    
+
+            
+
+
+
+        }
+
     }
-   
+    function hmm(){
+        alert(localStorage.il+" "+localStorage.todo);
 
+
+    }
+
+
+
+});
 
