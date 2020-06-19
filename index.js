81ee3a514f3fc5ceb602066b993c69013e61299b
Pcarioca <63117204+Pcarioca@users.noreply.github.com>
2020-06-19 14:38:49 +0300

    MERGE!

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 
+    //Restabileste TO-tu-urile din localStorage.
+
+
+    var count=localStorage.getItem("nr");
+        
+        count=parseInt(count,10);
+
+      
+
+        
+        
+        var char2 = "";
+        var todo3 = "";
+        for(var k=1;k<=count;k++){
+            todo3 = localStorage.getItem(k.toString());
+            
+            var para = document.createElement("p");
+            var node = document.createTextNode(todo3);
+            para.appendChild(node);
+            var element = document.getElementById("div1");
+            element.appendChild(para);
+            var br = document.createElement("br");
+            element.appendChild(br);
+
+
+
+
+
+            
+        }
+
+
+
+    //sfarsit
+
+
+
+
+
 
     var todo = [];
     document.getElementById("buton").onclick = add;
     document.getElementById("button").onclick = storage;
-    
+
     var i = 0;
-    var cookie="";
     function add() {
 
         var da = document.getElementById("input").value;
@@ -26,16 +64,28 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById("input").value = "";
             localStorage.setItem(i.toString(),da);
             localStorage.setItem("nr",i.toString());
+            
+
         }
 
     }
+
     function storage(){
         var counter=localStorage.getItem("nr");
+        
         counter=parseInt(counter,10);
+
+        //pana aici e ok
+
+        
+        
         var char = "";
-        for(var j=0;j<=counter;i++){
-            var todo2 = localStorage.getItem(j.toString());
+        var todo2 = "";
+        for(var j=0;j<=counter;j++){
+            todo2 = localStorage.getItem(j.toString());
             char+=todo2+"\n";
+
+
 
         }
         alert(char);
@@ -43,6 +93,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     }
+
+    
 
 
 
