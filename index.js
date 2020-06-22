713d4473799f80e80ef0ae6a4be0d22456d609be
Pcarioca <63117204+Pcarioca@users.noreply.github.com>
2020-06-22 12:28:07 +0300

    mdea

--- index.js
+++ index.js
@@ -2,22 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById("input").onkeypress = tasta;
 
-    if (localStorage.getItem("nr") == null||localStorage.getItem("nr") == undefined)     
+    if (localStorage.getItem("nr") == null || localStorage.getItem("nr") == undefined)
     /* Daca utilizatorul nu a mai folosit platforma TO-do, atunci
      *@getItem("nr") ar trebui sa nu existe deci sa initializeze i cu 0;
      *altfel, ar trebui sa aiba o valoare sub forma de String si sa continue
      *de unde a ramas cu numaratoarea.
-     */
-        {i = 0;}
-    else
-        {i = parseInt(localStorage.getItem("nr"), 10)+1;}
+     */ { i = 0; }
+    else { i = parseInt(localStorage.getItem("nr"), 10); }
 
 
     console.log("prima data i=" + i);
     console.log("parseInt(localStorage.getItem('nr'),10)=", + parseInt(localStorage.getItem("nr"), 10));
-    console.log("localStorage.getItem('nr')="+localStorage.getItem("nr"));
+    console.log("localStorage.getItem('nr')=" + localStorage.getItem("nr"));
 
-    //  !!  Restabileste TO-tu-urile din localStorage.  !!  (functioneaza perfect)
+    //  !!  Restabileste TO-do-urile din localStorage.  !!  (functioneaza perfect)(sau lasa)
 
 
     var count = parseInt(localStorage.getItem("nr"), 10);
@@ -29,18 +27,36 @@ document.addEventListener('DOMContentLoaded', function () {
     for (var k = 1; k <= count; k++) {
         console.log(" in primul for k=" + k);
         todo3 = localStorage.getItem(k.toString());
-        if(todo3==null) continue;
+        if (todo3 == null) continue;
         console.log("localStorage.getItem(k.toString())=" + localStorage.getItem(k.toString()));
-
         var para = document.createElement("p");
         var node = document.createTextNode(todo3);
         para.appendChild(node);
         var element = document.getElementById("div1");
         element.appendChild(para);
+    
+        var unde = document.getElementById("remover");
+        para.setAttribute("id","p"+k.toString());
+
         var br = document.createElement("br");
         element.appendChild(br);
+        br.setAttribute("id","br"+k.toString());
+        br.setAttribute("class","br");
+        
+
+
+        var RemoveBTN = document.createElement("button");
+            var minus = document.createTextNode("-");
+
+
+            var unde = document.getElementById("remover");
+            RemoveBTN.appendChild(minus);
+            unde.appendChild(RemoveBTN);
+            RemoveBTN.setAttribute("id","button"+k.toString());
+            RemoveBTN.setAttribute("class","minus btn-block btn"); 
+        
     }
-    //sfarsit
+       
 
 
 
@@ -56,8 +72,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     function add() {
-        if (localStorage.getItem("nr") == null||localStorage.getItem("nr") == undefined) 
-            i = 0;
+        if (localStorage.getItem("nr") == null || localStorage.getItem("nr") == undefined)
+            i = 1;
         else
             i = parseInt(localStorage.getItem("nr"), 10);
 
@@ -68,16 +84,30 @@ document.addEventListener('DOMContentLoaded', function () {
         if (da.localeCompare("")) { //functia se executa doar daca am bagat text propriu-zis
 
 
-            todo[i]=da;//introdu variabila in vector
+            todo[i] = da;//introdu variabila in vector
             var para = document.createElement("p");//creeaza un element @p in "div1" si desparte-le cu <br>.
             var node = document.createTextNode(todo[i]);
             para.appendChild(node);
             var element = document.getElementById("div1");
             element.appendChild(para);
+            let stringI=i.toString();
+            para.setAttribute("id","p"+stringI);
             var br = document.createElement("br");
+            
+            br.setAttribute("id","br"+stringI);
             element.appendChild(br);
 
 
+            var RemoveBTN = document.createElement("button");
+            var minus = document.createTextNode("-");
+
+
+            var unde = document.getElementById("remover");
+            RemoveBTN.appendChild(minus);
+            unde.appendChild(RemoveBTN);
+            RemoveBTN.setAttribute("id","button"+i.toString());
+            RemoveBTN.setAttribute("class","minus btn");
+            
             i++;
             document.getElementById("input").value = "";
 
@@ -86,19 +116,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             localStorage.setItem("nr", i.toString());//salveaza ultimul index introdus in vector, identificator "nr".        
 
-            //vreau sa adaug un buton de remove:
-
-            //    {
-
-            /*    var remover = document.createElement("button");
-                var minus = document.createTextNode("-");
-                remover.appendChild(minus);
-                var elem = document.getElementById("remover");
-                remover.appendChild(elem);
-                */
-
-            //    }
-            //sfarsit
+            
 
 
 
@@ -135,17 +153,11 @@ document.addEventListener('DOMContentLoaded', function () {
         location.reload();
 
     }
-    function tasta(){
-        
-            var char = event.which || event.keyCode;
-            if(char==13) add();
-          
+    function tasta() {
+
+        var char = event.which || event.keyCode;
+        if (char == 13) add();
+
     }
 
-
-
-
-
-
 });
-
