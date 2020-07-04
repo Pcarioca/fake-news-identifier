37fc88687bd5aaf7affeaed2ea83ff103a6d9bc3
Pcarioca <63117204+Pcarioca@users.noreply.github.com>
2020-07-04 16:17:10 +0300

    mda

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById("input").onkeypress = tasta;
+    document.getElementsByClassName("minus").onkeypress = sterge;
 
     if (localStorage.getItem("nr") == null || localStorage.getItem("nr") == undefined)
     /* Daca utilizatorul nu a mai folosit platforma TO-do, atunci
@@ -10,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
      */ { i = 0; }
     else { i = parseInt(localStorage.getItem("nr"), 10); }
 
-
+    
     console.log("prima data i=" + i);
     console.log("parseInt(localStorage.getItem('nr'),10)=", + parseInt(localStorage.getItem("nr"), 10));
     console.log("localStorage.getItem('nr')=" + localStorage.getItem("nr"));
@@ -32,30 +33,50 @@ document.addEventListener('DOMContentLoaded', function () {
         var para = document.createElement("p");
         var node = document.createTextNode(todo3);
         para.appendChild(node);
+        Pdiv=document.createElement("div");
+
         var element = document.getElementById("div1");
+
+            var RemoveDIV=document.createElement("div");
+            element.appendChild(RemoveDIV);
+            
+            var RemoveBTN = document.createElement("button");
+            var minus = document.createTextNode("-");
+
+
         element.appendChild(para);
     
         var unde = document.getElementById("remover");
-        para.setAttribute("id","p"+k.toString());
+        para.setAttribute("id","button"+k.toString());
 
         var br = document.createElement("br");
-        element.appendChild(br);
+        
         br.setAttribute("id","br"+k.toString());
         br.setAttribute("class","br");
         
 
-
-        var RemoveBTN = document.createElement("button");
-            var minus = document.createTextNode("-");
+            
 
 
             var unde = document.getElementById("remover");
             RemoveBTN.appendChild(minus);
             unde.appendChild(RemoveBTN);
+            RemoveBTN.setAttribute("name",k.toString());
             RemoveBTN.setAttribute("id","button"+k.toString());
-            RemoveBTN.setAttribute("class","minus btn-block btn"); 
+            RemoveBTN.setAttribute("class","minus btn-block btn "+k.toString()); 
+           
+            unde.appendChild(RemoveDIV);
+            RemoveDIV.appendChild(RemoveBTN);
+            
+            listuta=document.getElementById("div1");
+            div2=document.createElement("div");
+            listuta.appendChild(div2);
+            div2.appendChild(para);
+
+
+            
         
-    }
+    }   
        
 
 
@@ -81,32 +102,38 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
         var da = document.getElementById("input").value;
-        if (da.localeCompare("")) { //functia se executa doar daca am bagat text propriu-zis
+        if (da.localeCompare("")) { //functia se executa doar daca am bagat text propriu-zis.
 
 
-            todo[i] = da;//introdu variabila in vector
-            var para = document.createElement("p");//creeaza un element @p in "div1" si desparte-le cu <br>.
+
+
+            todo[i] = da; //introdu variabila in vector.
+            var para = document.createElement("p"); //creeaza un element @p in "div1" si desparte-le cu <br>.
             var node = document.createTextNode(todo[i]);
             para.appendChild(node);
             var element = document.getElementById("div1");
             element.appendChild(para);
             let stringI=i.toString();
-            para.setAttribute("id","p"+stringI);
+            para.setAttribute("id","button"+stringI);
             var br = document.createElement("br");
             
             br.setAttribute("id","br"+stringI);
+            br.setAttribute("class","br"+stringI);
             element.appendChild(br);
 
 
             var RemoveBTN = document.createElement("button");
+
             var minus = document.createTextNode("-");
 
 
             var unde = document.getElementById("remover");
+            var div3 = document.getElementById("div1");
             RemoveBTN.appendChild(minus);
-            unde.appendChild(RemoveBTN);
-            RemoveBTN.setAttribute("id","button"+i.toString());
-            RemoveBTN.setAttribute("class","minus btn");
+            
+            RemoveBTN.setAttribute("name",k.toString());
+            RemoveBTN.setAttribute("class","minus btn-block btn "+i.toString());
+            
             
             i++;
             document.getElementById("input").value = "";
@@ -115,6 +142,10 @@ document.addEventListener('DOMContentLoaded', function () {
             //pe care trebuie sa il transform in String din cauza metodei @setItem().
 
             localStorage.setItem("nr", i.toString());//salveaza ultimul index introdus in vector, identificator "nr".        
+
+            div4=document.createElement("div");
+            div3.appendChild(div4);
+            div3.appendChild(RemoveBTN);
 
             
 
@@ -159,5 +190,22 @@ document.addEventListener('DOMContentLoaded', function () {
         if (char == 13) add();
 
     }
+    function sterge() {
+
+        alert("Am intrat in functie");
+        var deSters=RemoveBTN.name;
+        var nrDeSters=parseInt(deSters,10);
+        
+
+        
+        var bll = localStorage.getItem("nr");
+        bll--;
+        localStorage.setItem("nr", bll);
+        location.reload();
+
+            }
+    
+    
+
 
 });
