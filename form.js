59989b1c5700be32bcc0b58a7c9d7381cc603751
Pcarioca <63117204+Pcarioca@users.noreply.github.com>
2021-05-04 18:29:11 +0300

    Add files via upload

--- form.js
+++ form.js
@@ -1,17 +1,17 @@
-window.addEventListener("load", function () {
 
-    // Your web app's Firebase configuration
-    var firebaseConfig = {
-        apiKey: "AIzaSyCcbUzG0ON-BEmnVMIN_t2dpDi7K6RIY0g",
-        authDomain: "fakenews-aa8a7.firebaseapp.com",
-        projectId: "fakenews-aa8a7",
-        storageBucket: "fakenews-aa8a7.appspot.com",
-        messagingSenderId: "61332180525",
-        databaseURL: "https://fakenews-aa8a7-default-rtdb.firebaseio.com",
-        appId: "1:61332180525:web:540e7ded4c4141c03e72a4"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCcbUzG0ON-BEmnVMIN_t2dpDi7K6RIY0g",
+    authDomain: "fakenews-aa8a7.firebaseapp.com",
+    projectId: "fakenews-aa8a7",
+    storageBucket: "fakenews-aa8a7.appspot.com",
+    messagingSenderId: "61332180525",
+    databaseURL: "https://fakenews-aa8a7-default-rtdb.firebaseio.com",
+    appId: "1:61332180525:web:540e7ded4c4141c03e72a4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
     const database = firebase.database();
     const inputDOM = document.getElementById("input");
     const input = inputDOM;
@@ -23,266 +23,244 @@ window.addEventListener("load", function () {
     const trust = document.getElementById('trust');
     const untrust = document.getElementById('notrust');
     const site = document.getElementById('site');
-    const auth = firebase.auth();
-    function signUp() {
-        var email = document.getElementById("email");
-        var password = document.getElementById("password");
-        var success =
-            auth.createUserWithEmailAndPassword(email.value, password.value).then(() => {
-                alert("Ti-ai facut cont. Acum poti sa dai login")
-            }).catch((error) => {
-                alert(error);
-            })
+  const auth = firebase.auth();
+  function signUp(){
+      var email= document.getElementById("email");
+      var password = document.getElementById("password");
+
+       auth.createUserWithEmailAndPassword(email.value, password.value).then(()=>{
+           alert("sign up. Now sign in")
+       }).catch((error)=>{
+           alert(error);
+       })
 
 
-    }
-    
+  }
+  function checkSite(){
+    const url = inputSearch.value.replace(".", "");
+    database.ref('sites/'+ url).get().then(response =>{
+        info.setAttribute("style","display: flex");
+        upvoteSearch.innerHTML = `${response.val().upVotes} upvotes`;
+        downSearch.innerHTML = `${response.val().downVotes} downvotes`;
+        siteUrl.innerHTML = `${inputSearch.value}`
+        // console.log(response.val().googlero.upVotes)
 
-    function checkSite() {
-        const url = inputSearch.value.replace(".", "");
-        database.ref('sites/' + url).get().then(response => {
-            info.setAttribute("style", "display: flex");
-            upvoteSearch.innerHTML = `${response.val().upVotes} upvotes`;
-            downSearch.innerHTML = `${response.val().downVotes} downvotes`;
-            siteUrl.innerHTML = `${inputSearch.value}`
-            // console.log(response.val().googlero.upVotes)
-
-        })
-    }
-
-
-
-
-
-
-    function signIn() {
-        var email = document.getElementById("email");
-        var password = document.getElementById("password");
-
-        auth.signInWithEmailAndPassword(email.value, password.value)
-            .then((user) => {
-                console.log(user);
-                console.log(auth.currentUser);
-                window.location = 'home.html';
-            })
-            .catch((error) => {
-                alert(error.message);
-            });
-
-    }
+    })
+  }
 
 
 
 
 
 
-    function upScore() {
-        const user = auth.currentUser;
+  function signIn(){
+      var email = document.getElementById("email");
+      var password = document.getElementById("password");
 
-        database.ref('users/' + user.uid).get().then(response => {
-            if (response.val()) {
-                const websites = Object.values(response.val());
+    auth.signInWithEmailAndPassword(email.value, password.value)
+    .then((user)=>{
+        console.log(user);
+        console.log(auth.currentUser)
+        window.location = 'home.html'
+    })
+    .catch((error) =>{
+        alert(error.message);
+    });
 
-                if (websites.find(element => element === input.value)) {
+  }
 
-                    alert("Ai mai votat o data");
+
+
+
+
+
+  function upScore() {
+      const user = auth.currentUser;
+
+   database.ref('users/' + user.uid).get().then(response =>{
+       if(response.val()){
+        const websites = Object.values(response.val())
+
+        if(websites.find(element => element === input.value)){
+
+            alert("Ai mai votat o data")
+        }
+        else{
+           
+
+            database.ref('users/' + user.uid).push(
+                input.value
+            )
+            const url = input.value.replace(".", "");
+    database.ref('sites/'+ url).get().then(response =>{
+
+        if(response.val()){
+            let upVotes = response.val().upVotes;
+            let downVotes = response.val().downVotes;
+            site.innerHTML = `site-ul ${input}`
+         
+            console.log("line 101");
+            trust.innerHTML= `${upVotes + 1} au votat increderea site-ului`;
+            untrust.innerHTML= `${downVotes} nu au votat increderea site-ului`;
+
+            database.ref('sites/' + url).update(
+                {upVotes: upVotes+1
                 }
-                else {
+            )
+            inputDOM.value = "";
+         }
+        else{
+            site.innerHTML = `site-ul ${input.value}`
+                console.log("line 114")
+               
+                trust.innerHTML= `1 a votat increderea site-ului`;
+                untrust.innerHTML= `0 nu au votat increderea site-ului`;
 
-
-                    database.ref('users/' + user.uid).push(input.value);
-                    const url = input.value.replace(".", "");
-                    database.ref('sites/' + url).get().then(response => {
-
-                        if (response.val()) {
-                            let upVotes = response.val().upVotes;
-                            let downVotes = response.val().downVotes;
-                            site.innerHTML = `site-ul ${input}`;
-
-                            console.log("line 101");
-                            trust.innerHTML = `${upVotes + 1} au votat increderea site-ului`;
-                            untrust.innerHTML = `${downVotes} nu au votat increderea site-ului`;
-
-                            database.ref('sites/' + url).update(
-                                {
-                                    upVotes: upVotes + 1
-                                }
-                            )
-                            inputDOM.value = "";
-                        } else {
-                            site.innerHTML = `site-ul ${input.value}`;
-                            console.log("line 114");
-
-                            trust.innerHTML = `1 a votat increderea site-ului`;
-                            untrust.innerHTML = `0 nu au votat increderea site-ului`;
-
-                            database.ref('sites/' + url).update(
-                                {
-                                    upVotes: 1,
-                                    downVotes: 0
-                                }
-                            )
-                            inputDOM.value = "";
-                        }
-                    })
-
+            database.ref('sites/' + url).update(
+                {upVotes: 1,
+                downVotes:0
                 }
-            } else {
-                const url = input.value.replace(".", "");
-                database.ref('users/' + user.uid).push(
-                    input.value
+            )
+            inputDOM.value = "";
+        }
+    })
+
+        }
+       }
+       else{
+        const url = input.value.replace(".", "");
+        database.ref('users/' + user.uid).push(
+            input.value
+        )
+        database.ref('sites/'+ url).get().then(response =>{
+
+            if(response.val()){
+                let upVotes = response.val().upVotes;
+                let downVotes = response.val().downVotes;
+                site.innerHTML = `site-ul ${input.value}`
+                console.log("line 139")
+                trust.innerHTML= `${upVotes + 1} au votat increderea site-ului`;
+                untrust.innerHTML= `${downVotes} nu au votat increderea site-ului`;
+
+                database.ref('sites/' + url).update(
+                    {upVotes: upVotes+1
+                    }
                 )
-                database.ref('sites/' + url).get().then(response => {
+                inputDOM.value = "";
+             }
+             
+            else{
+                site.innerHTML = `site-ul ${input.value}`
+                console.log("line 152")
+                console.log(input);
+                trust.innerHTML= `1 a votat increderea site-ului`;
+                untrust.innerHTML= `0 nu au votat increderea site-ului`;
 
-                    if (response.val()) {
-                        let upVotes = response.val().upVotes;
-                        let downVotes = response.val().downVotes;
-                        site.innerHTML = `site-ul ${input.value}`
-                        console.log("line 139")
-                        trust.innerHTML = `${upVotes + 1} au votat increderea site-ului`;
-                        untrust.innerHTML = `${downVotes} nu au votat increderea site-ului`;
-
-                        database.ref('sites/' + url).update(
-                            {
-                                upVotes: upVotes + 1
-                            }
-                        )
-                        inputDOM.value = "";
+                database.ref('sites/' + url).update(
+                    {upVotes: 1,
+                    downVotes:0
                     }
-
-                    else {
-                        site.innerHTML = `site-ul ${input.value}`;
-                        console.log("line 152");
-                        console.log(input);
-                        trust.innerHTML = `1 a votat increderea site-ului`;
-                        untrust.innerHTML = `0 nu au votat increderea site-ului`;
-
-                        database.ref('sites/' + url).update(
-                            {
-                                upVotes: 1,
-                                downVotes: 0
-                            }
-                        );
-                        inputDOM.value = "";
-                    }
-                })
-
+                )
+                inputDOM.value = "";
             }
-
         })
 
-    }
+       }
 
-    function downScore() {
-        const user = auth.currentUser;
+   })
 
-        database.ref('users/' + user.uid).get().then(response => {
-            if (response.val()) {
-                const websites = Object.values(response.val())
+  }
 
-                if (websites.find(element => element === input.value)) {
+  function downScore() {
+    const user = auth.currentUser;
 
-                    alert("Ai mai votat o data");
+   database.ref('users/' + user.uid).get().then(response =>{
+       if(response.val()){
+        const websites = Object.values(response.val())
+
+        if(websites.find(element => element === input.value)){
+
+            alert("Ai mai votat o data")
+        }
+        else{
+            site.innerHTML = `site-ul ${input.value}`
+
+            database.ref('users/' + user.uid).push(
+                input.value
+            )
+            const url = input.value.replace(".", "");
+    database.ref('sites/'+ url).get().then(response =>{
+
+        if(response.val()){
+            let upVotes = response.val().upVotes;
+            let downVotes = response.val().downVotes;
+            site.innerHTML = `site-ul ${input.value}`
+            trust.innerHTML= `${upVotes} au votat increderea site-ului`;
+            untrust.innerHTML= `${downVotes + 1} nu au votat increderea site-ului`;
+
+            database.ref('sites/' + url).update(
+                {downVotes: downVotes+1
                 }
-                else {
-                    site.innerHTML = `site-ul ${input.value}`;
+            )
+            inputDOM.value = "";
+         }
+        else{
+            site.innerHTML = `site-ul ${input.value}`
+            trust.innerHTML= `0 au votat increderea site-ului`;
+            untrust.innerHTML= `1 nu au votat increderea site-ului`;
 
-                    database.ref('users/' + user.uid).push(
-                        input.value
-                    )
-                    const url = input.value.replace(".", "");
-                    database.ref('sites/' + url).get().then(response => {
-
-                        if (response.val()) {
-                            let upVotes = response.val().upVotes;
-                            let downVotes = response.val().downVotes;
-                            site.innerHTML = `site-ul ${input.value}`
-                            trust.innerHTML = `${upVotes} au votat increderea site-ului`;
-                            untrust.innerHTML = `${downVotes + 1} nu au votat increderea site-ului`;
-
-                            database.ref('sites/' + url).update(
-                                {
-                                    downVotes: downVotes + 1
-                                }
-                            );
-                            inputDOM.value = "";
-                        }
-                        else {
-                            site.innerHTML = `site-ul ${input.value}`;
-                            trust.innerHTML = `0 au votat increderea site-ului`;
-                            untrust.innerHTML = `1 nu au votat increderea site-ului`;
-
-                            database.ref('sites/' + url).update(
-                                {
-                                    upVotes: 0,
-                                    downVotes: 1
-                                }
-                            );
-                            inputDOM.value = "";
-                        }
-                    })
+            database.ref('sites/' + url).update(
+                {upVotes: 0,
+                downVotes:1
                 }
-            }
-            else {
-                const url = input.value.replace(".", "");
-                database.ref('users/' + user.uid).push(
-                    input.value
-                );
-                database.ref('sites/' + url).get().then(response => {
+            )
+            inputDOM.value = "";
+        }
+    })
+        }
+       }
+       else{
+        const url = input.value.replace(".", "");
+        database.ref('users/' + user.uid).push(
+            input.value
+        )
+        database.ref('sites/'+ url).get().then(response =>{
 
-                    if (response.val()) {
-                        let upVotes = response.val().upVotes;
-                        let downVotes = response.val().downVotes;
-                        site.innerHTML = `site-ul ${input.value}`
-                        trust.innerHTML = `${upVotes} au votat increderea site-ului`;
-                        untrust.innerHTML = `${downVotes + 1} nu au votat increderea site-ului`;
+            if(response.val()){
+                let upVotes = response.val().upVotes;
+                let downVotes = response.val().downVotes;
+                site.innerHTML = `site-ul ${input.value}`
+                trust.innerHTML= `${upVotes } au votat increderea site-ului`;
+                untrust.innerHTML= `${downVotes + 1} nu au votat increderea site-ului`;
 
-                        database.ref('sites/' + url).update(
-                            {
-                                downVotes: downVotes + 1
-                            }
-                        )
-                        inputDOM.value = "";
+                database.ref('sites/' + url).update(
+                    {downVotes: downVotes+1
                     }
-                    else {
-                        site.innerHTML = `site-ul ${input.value}`
-                        trust.innerHTML = `0 a votat increderea site-ului`;
-                        untrust.innerHTML = `1 nu au votat increderea site-ului`;
+                )
+                inputDOM.value = "";
+             }
+            else{
+                site.innerHTML = `site-ul ${input.value}`
+                trust.innerHTML= `0 a votat increderea site-ului`;
+                untrust.innerHTML= `1 nu au votat increderea site-ului`;
 
-                        database.ref('sites/' + url).update(
-                            {
-                                upVotes: 0,
-                                downVotes: 1
-                            }
-                        );
-                        inputDOM.value = "";
+                database.ref('sites/' + url).update(
+                    {upVotes: 0,
+                    downVotes:1
                     }
-                });
-
+                )
+                inputDOM.value = "";
             }
+        })
 
-        });
+       }
+
+   })
 
 
-    }
-    function signOut() {
-        auth.signOut();
-        window.location = 'index.html';
-        alert("Signed Out");
-    }
-    //document.getElementById("signIn").addEventListener("click", signIn);
-    //document.getElementById("signUp").addEventListener("click", signUp);
-    //document.getElementById("button1").addEventListener("click", signOut);
-
-    //incepem sa codam captcha
-    var onloadCallback = function () {
-        // Renders the HTML element with id 'example1' as a reCAPTCHA widget.
-        // The id of the reCAPTCHA widget is assigned to 'widgetId1'.
-        widgetId1 = grecaptcha.render('widget', {
-            'sitekey': '6LcTw7caAAAAAEAClZxvKgnhULHdBzPs-Npg_JZc',
-            'theme': 'dark'
-        });
-
-    };
-
-});
+  }
+  function signOut(){
+      auth.signOut();
+      window.location='index.html';
+      alert("Signed Out");
+  }
---
+++ index.js
@@ -0,0 +1,211 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.getElementById("input").onkeypress = tasta;
+    document.getElementsByClassName("minus").onkeypress = sterge;
+
+    if (localStorage.getItem("nr") == null || localStorage.getItem("nr") == undefined)
+    /* Daca utilizatorul nu a mai folosit platforma TO-do, atunci
+     *@getItem("nr") ar trebui sa nu existe deci sa initializeze i cu 0;
+     *altfel, ar trebui sa aiba o valoare sub forma de String si sa continue
+     *de unde a ramas cu numaratoarea.
+     */ { i = 0; }
+    else { i = parseInt(localStorage.getItem("nr"), 10); }
+
+    
+    console.log("prima data i=" + i);
+    console.log("parseInt(localStorage.getItem('nr'),10)=", + parseInt(localStorage.getItem("nr"), 10));
+    console.log("localStorage.getItem('nr')=" + localStorage.getItem("nr"));
+
+    //  !!  Restabileste TO-do-urile din localStorage.  !!  (functioneaza perfect)(sau lasa)
+
+
+    var count = parseInt(localStorage.getItem("nr"), 10);
+
+
+
+    var char2 = "";
+    var todo3 = "";
+    for (var k = 1; k <= count; k++) {
+        console.log(" in primul for k=" + k);
+        todo3 = localStorage.getItem(k.toString());
+        if (todo3 == null) continue;
+        console.log("localStorage.getItem(k.toString())=" + localStorage.getItem(k.toString()));
+        var para = document.createElement("p");
+        var node = document.createTextNode(todo3);
+        para.appendChild(node);
+        Pdiv=document.createElement("div");
+
+        var element = document.getElementById("div1");
+
+            var RemoveDIV=document.createElement("div");
+            element.appendChild(RemoveDIV);
+            
+            var RemoveBTN = document.createElement("button");
+            var minus = document.createTextNode("-");
+
+
+        element.appendChild(para);
+    
+        var unde = document.getElementById("remover");
+        para.setAttribute("id","button"+k.toString());
+
+        var br = document.createElement("br");
+        
+        br.setAttribute("id","br"+k.toString());
+        br.setAttribute("class","br");
+        
+
+            
+
+
+            var unde = document.getElementById("remover");
+            RemoveBTN.appendChild(minus);
+            unde.appendChild(RemoveBTN);
+            RemoveBTN.setAttribute("name",k.toString());
+            RemoveBTN.setAttribute("id","button"+k.toString());
+            RemoveBTN.setAttribute("class","minus btn-block btn "+k.toString()); 
+           
+            unde.appendChild(RemoveDIV);
+            RemoveDIV.appendChild(RemoveBTN);
+            
+            listuta=document.getElementById("div1");
+            div2=document.createElement("div");
+            listuta.appendChild(div2);
+            div2.appendChild(para);
+
+
+            
+        
+    }   
+       
+
+
+
+
+
+
+    var todo = [];
+    //document.getElementById("buton").onclick = add;
+    //document.getElementById("button").onclick = storage;   !!!ASTEA NU MAI SUNT UTILE
+    //document.getElementById("vanish").onclick = vanish;
+
+
+
+
+    function add() {
+        if (localStorage.getItem("nr") == null || localStorage.getItem("nr") == undefined)
+            i = 1;
+        else
+            i = parseInt(localStorage.getItem("nr"), 10);
+
+        console.log("i in add()=" + i);
+
+
+        var da = document.getElementById("input").value;
+        if (da.localeCompare("")) { //functia se executa doar daca am bagat text propriu-zis.
+
+
+
+
+            todo[i] = da; //introdu variabila in vector.
+            var para = document.createElement("p"); //creeaza un element @p in "div1" si desparte-le cu <br>.
+            var node = document.createTextNode(todo[i]);
+            para.appendChild(node);
+            var element = document.getElementById("div1");
+            element.appendChild(para);
+            let stringI=i.toString();
+            para.setAttribute("id","button"+stringI);
+            var br = document.createElement("br");
+            
+            br.setAttribute("id","br"+stringI);
+            br.setAttribute("class","br"+stringI);
+            element.appendChild(br);
+
+
+            var RemoveBTN = document.createElement("button");
+
+            var minus = document.createTextNode("-");
+
+
+            var unde = document.getElementById("remover");
+            var div3 = document.getElementById("div1");
+            RemoveBTN.appendChild(minus);
+            
+            RemoveBTN.setAttribute("name",k.toString());
+            RemoveBTN.setAttribute("class","minus btn-block btn "+i.toString());
+            
+            
+            i++;
+            document.getElementById("input").value = "";
+
+            localStorage.setItem(i.toString(), da); //salveaza ultimul element adaugat sub identificatorul @i 
+            //pe care trebuie sa il transform in String din cauza metodei @setItem().
+
+            localStorage.setItem("nr", i.toString());//salveaza ultimul index introdus in vector, identificator "nr".        
+
+            div4=document.createElement("div");
+            div3.appendChild(div4);
+            div3.appendChild(RemoveBTN);
+
+            
+
+
+
+        }
+
+    }
+
+    function storage() { //functie care arata intr-o alerta ce e salvat in @localStorage
+        var counter = localStorage.getItem("nr");
+
+        counter = parseInt(counter, 10);
+
+
+
+
+
+        var char = "";
+        var todo2 = "";
+        for (var j = 0; j <= counter; j++) {
+            todo2 = localStorage.getItem(j.toString());
+            char += todo2 + "\n";
+
+
+
+        }
+        alert(char);
+
+
+
+    }
+    function vanish() {
+
+        localStorage.clear();
+        location.reload();
+
+    }
+    function tasta() {
+
+        var char = event.which || event.keyCode;
+        if (char == 13) add();
+
+    }
+    function sterge() {
+
+        alert("Am intrat in functie");
+        var deSters=RemoveBTN.name;
+        var nrDeSters=parseInt(deSters,10);
+        
+
+        
+        var bll = localStorage.getItem("nr");
+        bll--;
+        localStorage.setItem("nr", bll);
+        location.reload();
+
+            }
+    
+    
+
+
+});
